6de8526176245793b4a942764d60411101a7a247
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2024-10-30 13:32:34 +0100

    Init commit samordning-personoppslag

---
+++ build.gradle.kts
@@ -0,0 +1,114 @@
+import h2DbVersion
+import hibernateCoreVersion
+import jacksonkotlinVersion
+import jakartaAnnotationApiVersion
+import jakartaInjectApiVersion
+import junitplatformVersion
+import kotlinVersion
+import logstashlogbackVersion
+import mockOAuth2ServerVersion
+import mockkVersion
+import oracle11Version
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import prometeusVersion
+import slf4jVersion
+import springbootVersion
+import springframeworkbomVersion
+import springkafkaVersion
+import springmockkVersion
+import springwebmvcpac4jVersion
+import tokensupportVersion
+import tokensupporttestVersion
+
+plugins {
+    val pluginSpringBootVersion = "3.3.2"
+    val pluginKotlinVersion = "2.0.20"
+
+    kotlin("jvm") version pluginKotlinVersion
+    kotlin("plugin.spring") version pluginKotlinVersion
+    kotlin("plugin.jpa") version pluginKotlinVersion
+    id("org.springframework.boot") version pluginSpringBootVersion
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.owasp.dependencycheck") version "10.0.4"
+}
+
+group = "no.nav.pensjon"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // Spring Boot & Framework
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:$springbootVersion"))
+    implementation("org.springframework.boot:spring-boot-starter-web:$springbootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-aop:$springbootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:$springbootVersion")
+    implementation("org.springframework.boot:spring-boot-actuator:$springbootVersion")
+    implementation(platform("org.springframework:spring-framework-bom:$springframeworkbomVersion"))
+
+    //spring boot 3.0 jakaera-api
+    implementation("jakarta.annotation:jakarta.annotation-api:$jakartaAnnotationApiVersion")
+    implementation("jakarta.inject:jakarta.inject-api:$jakartaInjectApiVersion")
+
+    // Kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonkotlinVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonkotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+
+    // Token support Azuread, Oidc
+    implementation("no.nav.security:token-validation-spring:$tokensupportVersion")
+    implementation("no.nav.security:token-validation-jaxrs:$tokensupportVersion")
+    implementation("no.nav.security:token-client-spring:$tokensupportVersion")
+    // Only used for starting up locally testing
+    implementation("no.nav.security:token-validation-test-support:$tokensupporttestVersion")
+    testImplementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
+    testImplementation("no.nav.security:token-validation-spring-test:$tokensupportVersion")
+
+    // Logging
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashlogbackVersion")
+    implementation("org.slf4j:jcl-over-slf4j:$slf4jVersion")
+    // Micrometer
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometeusVersion")
+
+    // test
+    testImplementation("com.ninja-squad:springmockk:$springmockkVersion")
+    testImplementation("org.pac4j:spring-webmvc-pac4j:$springwebmvcpac4jVersion")
+
+    // mock - test
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("org.junit.platform:junit-platform-suite-api:$junitplatformVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springbootVersion")
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+        failFast = true
+        testLogging {
+            events("passed", "skipped", "failed")
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        }
+    }
+
+
+    withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+            freeCompilerArgs.add("-Xjsr305=strict")
+        }
+    }
+
+    withType<Jar> {
+        archiveBaseName.set("samordning-personoppslag")
+    }
+
+    withType<Wrapper> {
+        gradleVersion = "8.9"
+    }
+
+}
