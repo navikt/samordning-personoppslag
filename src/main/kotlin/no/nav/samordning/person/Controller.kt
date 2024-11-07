a7298bdba2c4a293c0b26a7fdf4eb635ea88001a
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2024-11-07 11:49:42 +0100

    forbedret test

--- src/main/kotlin/no/nav/samordning/person/Controller.kt
+++ src/main/kotlin/no/nav/samordning/person/Controller.kt
@@ -2,23 +2,31 @@ package no.nav.samordning.person
 
 import jakarta.validation.constraints.Digits
 import no.nav.samordning.person.pdl.PersonService
+import no.nav.samordning.person.pdl.PersonoppslagException
 import no.nav.samordning.person.pdl.model.NorskIdent
 import no.nav.samordning.person.pdl.model.PdlPerson
-import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.Protected
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 class Controller(private val personService: PersonService) {
 
 
     @GetMapping("/api/person")
-    @ProtectedWithClaims("entraid")
+    //@ProtectedWithClaims("entraid")
+    @Protected
     fun hentPerson(@RequestHeader("fnr") @Digits(integer = 11, fraction = 0) fnr: String) : ResponseEntity<PdlPerson?> {
 
-        return ResponseEntity.ok().body(personService.hentPerson(NorskIdent(fnr)))
+        try {
+            return ResponseEntity.ok().body(personService.hentPerson(NorskIdent(fnr)))
+        } catch (pe: PersonoppslagException) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, pe.message)
+        }
 
     }
 
--- src/test/kotlin/no/nav/samordning/person/ControllerMVCTest.kt
+++ src/test/kotlin/no/nav/samordning/person/ControllerMVCTest.kt
@@ -4,7 +4,36 @@ import com.ninjasquad.springmockk.MockkBean
 import io.mockk.clearAllMocks
 import io.mockk.every
 import no.nav.samordning.person.pdl.PdlConfigurationTest
+import no.nav.samordning.person.pdl.model.Adressebeskyttelse
+import no.nav.samordning.person.pdl.model.AdressebeskyttelseGradering
+import no.nav.samordning.person.pdl.model.Bostedsadresse
+import no.nav.samordning.person.pdl.model.Doedsfall
+import no.nav.samordning.person.pdl.model.Endring
+import no.nav.samordning.person.pdl.model.Endringstype
+import no.nav.samordning.person.pdl.model.Familierelasjonsrolle
+import no.nav.samordning.person.pdl.model.Foedsel
+import no.nav.samordning.person.pdl.model.Folkeregistermetadata
+import no.nav.samordning.person.pdl.model.ForelderBarnRelasjon
+import no.nav.samordning.person.pdl.model.GeografiskTilknytning
+import no.nav.samordning.person.pdl.model.GeografiskTilknytningResponse
+import no.nav.samordning.person.pdl.model.GeografiskTilknytningResponseData
+import no.nav.samordning.person.pdl.model.GtType
+import no.nav.samordning.person.pdl.model.HentIdenter
+import no.nav.samordning.person.pdl.model.HentPerson
 import no.nav.samordning.person.pdl.model.HentPersonResponse
+import no.nav.samordning.person.pdl.model.HentPersonResponseData
+import no.nav.samordning.person.pdl.model.IdentGruppe
+import no.nav.samordning.person.pdl.model.IdentInformasjon
+import no.nav.samordning.person.pdl.model.IdenterDataResponse
+import no.nav.samordning.person.pdl.model.IdenterResponse
+import no.nav.samordning.person.pdl.model.Kjoenn
+import no.nav.samordning.person.pdl.model.KjoennType
+import no.nav.samordning.person.pdl.model.Metadata
+import no.nav.samordning.person.pdl.model.Navn
+import no.nav.samordning.person.pdl.model.Sivilstand
+import no.nav.samordning.person.pdl.model.Sivilstandstype
+import no.nav.samordning.person.pdl.model.Statsborgerskap
+import no.nav.samordning.person.pdl.model.Vegadresse
 import no.nav.security.mock.oauth2.MockOAuth2Server
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.AfterEach
@@ -18,6 +47,8 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForObject
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
 
@@ -43,11 +74,29 @@ internal class ControllerMVCTest {
     }
 
     @Test
-    fun `correct call with valid fnr will return response full persondata`() {
+    fun `correct call with valid fnr response return persondata`() {
         val token = issueSystembrukerToken(roles = listOf("SAM", "BRUKER"))
 
-        //every { pdlRestTemplate.postForObject<HentPersonResponse>(any(), any(), HentPersonResponse::class) } returns HentPersonResponse()
-        every { pdlRestTemplate.postForObject<HentPersonResponse>(any(), any(), HentPersonResponse::class) } returns HentPersonResponse()
+        val hentPersonResponse = HentPersonResponse(data = HentPersonResponseData(hentPerson = mockHentAltPerson()))
+
+        val identerResponse = IdenterResponse(data = IdenterDataResponse(
+                hentIdenter = HentIdenter(
+                    identer = listOf(
+                        IdentInformasjon("25078521492", IdentGruppe.FOLKEREGISTERIDENT),
+                        IdentInformasjon("100000000000053", IdentGruppe.AKTORID)
+                    )
+                )
+            )
+        )
+
+        val geografiskTilknytningResponse = GeografiskTilknytningResponse(
+            data = GeografiskTilknytningResponseData(geografiskTilknytning = GeografiskTilknytning(GtType.KOMMUNE, "0301", null, null))
+        )
+
+
+        every { pdlRestTemplate.postForObject<HentPersonResponse>(any(), any(), HentPersonResponse::class) } returns hentPersonResponse
+        every { pdlRestTemplate.postForObject<IdenterResponse>(any(), any(), IdenterResponse::class) } returns identerResponse
+        every { pdlRestTemplate.postForObject<GeografiskTilknytningResponse>(any(), any(), GeografiskTilknytningResponse::class) } returns geografiskTilknytningResponse
 
         mvc.get("/api/person") {
                 header("fnr", "1213123123")
@@ -55,7 +104,13 @@ internal class ControllerMVCTest {
             }
             .andDo { print() }
             .andExpect { status { isOk() }
-        }
+
+                jsonPath("$.navn.fornavn") { value("Fornavn") }
+                jsonPath("$.navn.etternavn") { value("Etternavn") }
+                jsonPath("$.identer.size()") { value(2) }
+                jsonPath("$.geografiskTilknytning.gtKommune") { value("0301") }
+            }
+
 
     }
 
@@ -75,5 +130,45 @@ internal class ControllerMVCTest {
                 ),
                 expiry = 360
             ).serialize()
+
+
+    private fun mockHentAltPerson() = HentPerson(
+        adressebeskyttelse = listOf(Adressebeskyttelse(AdressebeskyttelseGradering.UGRADERT)),
+        bostedsadresse = listOf(
+            Bostedsadresse(
+                LocalDateTime.of(2020, 10, 5, 10,5,2),
+                LocalDateTime.of(2030, 10, 5, 10, 5, 2),
+                Vegadresse("TESTVEIEN","1020","A","0234", "231", null),
+                null,
+                mockMeta()
+            )
+        ),
+        oppholdsadresse = emptyList(),
+        navn = listOf(Navn("Fornavn", "Mellomnavn", "Etternavn", null, null, null, mockMeta())),
+        statsborgerskap = listOf(Statsborgerskap("NOR", LocalDate.of(2010, 7,7), LocalDate.of(2020, 10, 10), mockMeta())),
+        foedsel = listOf(Foedsel(LocalDate.of(2000,10,3), "NOR", "OSLO", 2020, Folkeregistermetadata(LocalDateTime.of(2020, 10, 5, 10,5,2)), mockMeta())),
+        kjoenn = listOf(Kjoenn(KjoennType.KVINNE, Folkeregistermetadata(LocalDateTime.of(2020, 10, 5, 10,5,2)), mockMeta())),
+        doedsfall = listOf(Doedsfall(LocalDate.of(2020, 10,10), Folkeregistermetadata(LocalDateTime.of(2020, 10, 5, 10,5,2)), mockMeta())),
+        forelderBarnRelasjon = listOf(ForelderBarnRelasjon("101010", Familierelasjonsrolle.BARN, Familierelasjonsrolle.MOR, mockMeta())),
+        sivilstand = listOf(Sivilstand(Sivilstandstype.GIFT, LocalDate.of(2010, 10,10), "1020203010", mockMeta())),
+        kontaktadresse = emptyList(),
+        kontaktinformasjonForDoedsbo = emptyList()
+    )
+
+    private fun mockMeta(registrert: LocalDateTime = LocalDateTime.of(2010, 4,1, 10, 2, 14)): no.nav.samordning.person.pdl.model.Metadata {
+        return Metadata(
+            listOf(
+                Endring(
+                    "TEST",
+                    registrert,
+                    "Test",
+                    "Kilde test",
+                    Endringstype.OPPRETT
+                )),
+            false,
+            "Test",
+            "acbe1a46-e3d1"
+        )
+    }
 
 }
