634af6afdb6afdaace2b383f4ef1f5c253bf2771
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2024-11-12 14:32:40 +0100

    PES-565: fjerne ubrukt felter i kodeverk.

--- src/main/kotlin/no/nav/samordning/kodeverk/Kodeverk.kt
+++ src/main/kotlin/no/nav/samordning/kodeverk/Kodeverk.kt
@@ -17,63 +17,30 @@ data class Postnummer(
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class KodeverkResponse(
     val navn: String,
-//    val eier: String,
-//    val forvaltere: List<String>,
-//    val senestePubliseringstidspunkt: String,
-//    val status: KodeverkStatusEnum,
-//    val tilgjengeligeSpraak: List<String>,
     val koder: List<KodeverkKode>
 )
 
-
-enum class KodeverkStatusEnum {
-    UTKAST,
-    STANDARD;
-}
-
 enum class KodeStatusEnum {
-    NY,
-    OPPDATERT,
-    PUBLISERT,
     SLETTET;
 }
 
-//LanguageCode = "nb" | "nn" | "en" | "se";
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class KodeverkKode(
- //   val id: Long,
     val navn: String,
-//    val senestePubliseringstidspunkt: String,
-//    val kodeverk: String,
     val status: KodeStatusEnum,
     val betydning: KodeverkBetydning
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class KodeverkBetydning(
-//    val id: Long,
-//    val senestePubliseringstidspunkt: String,
-//    val status: KodeStatusEnum,
-//    val gyldigFra: LocalDate,
-//    val gammelGyldigFra: LocalDate?,
-//    val gyldigTil: LocalDate?,
-//    val gammelGyldigTil: LocalDate?,
     val beskrivelse: KodeverkBeskrivelse
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class KodeverkBeskrivelse(
-//    val id: Long,
-//    val senestePubliseringstidspunkt: String,
-//    val spraak: String?,
-//    val status: KodeStatusEnum,
-//    val tekst: String?,
+    val tekst: String?,
     val term: String
-//    val gammelTerm: String?,
-//    val gammelTekst: String?
 )
-
 
 
 class KodeverkException(message: String) : ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, message)
