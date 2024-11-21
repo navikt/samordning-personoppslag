0ef817a984d20668746d0cbdcc31224059d627e1
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2024-11-21 11:40:05 +0100

    PES-561: legge til Behandlingsnummer

--- src/main/kotlin/no/nav/samordning/person/pdl/Behandlingsnummer.kt
+++ src/main/kotlin/no/nav/samordning/person/pdl/Behandlingsnummer.kt
@@ -3,19 +3,20 @@ package no.nav.samordning.person.pdl
 import com.fasterxml.jackson.annotation.JsonValue
 
 /**
- * https://behandlingskatalog.nais.adeo.no/process/system/EESSI_PENSJON/e0ba9743-28d6-496b-bc6b-9627fa0f3843
- * B367, Alderspensjon: Utveksling av informasjon med EØS-land og Sveits
- * B436, Barnepensjon: Utveksling av informasjon med EØS-land og Sveits
- * B432, Gjenlevendepensjon og overgangsstønad: Utveksling av informasjon med EØS-land og Sveits
- * B358, Medlemskap og lovvalg: Vurdering av lovvalg og avgift etter trygdeforordningen
- * B528, Statistikk: Oversendelse av pensjonsdata fra EESSI til DVH
- * B469, Statistikk: Pensjonsstatistikk
- * B419, Uføretrygd: Utveksling av informasjon med EU/EØS-land
+ * https://behandlingskatalog.intern.nav.no/process/team/5967460b-e9bd-4e63-9544-db209136c867/d0cf6f79-3c8b-4b35-91bf-5b1e74516fb0
+ * B361, Uføretrygd: Samordning med offentlige TP-ordninger
+ * B354, Alderspensjon: Samordning med offentlige TP-ordninger
+ * B248, Gjenlevendepensjon og overgangsstønad: Samordning med offentlige TP-ordninger
+ * B430, AFP i statlig og kommunal sektor: Samordning med offentlig tjenestepensjon
+ * B429, AFP i privat sektor: Samordning med offentlige tjenestepensjon
+ * B427, Barnepensjon: Samordning med offentlige TP-ordninger
+
  */
 enum class Behandlingsnummer(@JsonValue val nummer: String) {
-      SAMORDNING_SAMHANDLER("ETT_ELLER_ANNET_NURM")
-//    UFORETRYGD("B419"),
-//    ALDERPENSJON("B367"),
-//    BARNEPENSJON("B436"),
-//    GJENLEV_OG_OVERGANG("B432"),
+      UFORETRYGD("B361"),
+      ALDERPENSJON("B354"),
+      GJENLEV_OG_OVERGANG("B428"),
+      AFP_STATLIG_KOMMUNAL("B430"),
+      AFP_PRIVAT_SEKTOR("B429"),
+      BARNEPENSJON("B427")
 }
--- src/main/kotlin/no/nav/samordning/person/pdl/PdlConfiguration.kt
+++ src/main/kotlin/no/nav/samordning/person/pdl/PdlConfiguration.kt
@@ -5,6 +5,7 @@ import no.nav.common.token_client.cache.CaffeineTokenCache
 import no.nav.samordning.interceptor.AzureAdMachineToMachineTokenClientHttpRequestInterceptor
 import no.nav.samordning.interceptor.IOExceptionRetryInterceptor
 import no.nav.samordning.metrics.MetricsHelper
+import no.nav.samordning.person.pdl.Behandlingsnummer.*
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -14,7 +15,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpRequest
-import org.springframework.http.client.*
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.client.DefaultResponseErrorHandler
 import org.springframework.web.client.RestTemplate
 
@@ -53,12 +56,12 @@ class PdlConfiguration(@Autowired(required = false) private val metricsHelper: M
             request.headers[HttpHeaders.CONTENT_TYPE] = "application/json"
             request.headers["Tema"] = "PEN"
             request.headers["Behandlingsnummer"] =
-                Behandlingsnummer.SAMORDNING_SAMHANDLER.nummer
-//                Behandlingsnummer.ALDERPENSJON.nummer + "," +
-//                Behandlingsnummer.UFORETRYGD.nummer + "," +
-//                Behandlingsnummer.GJENLEV_OG_OVERGANG.nummer + "," +
-//                Behandlingsnummer.BARNEPENSJON.nummer
-
+                UFORETRYGD.nummer + "," +
+                ALDERPENSJON.nummer + "," +
+                GJENLEV_OG_OVERGANG.nummer + "," +
+                AFP_STATLIG_KOMMUNAL.nummer + "," +
+                AFP_PRIVAT_SEKTOR.nummer + "," +
+                BARNEPENSJON.nummer
             // [Borger, Saksbehandler eller System]
             logger.debug("PdlInterceptor httpRequest headers: ${request.headers}")
             return execution.execute(request, body)
