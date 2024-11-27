1cea26679ea44d1f431927cc59e260dae4b50c25
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2024-11-27 20:11:48 +0100

    PES-561 samkjøre resultat i hentPersonalia i SAM mot mapping av PersonSamordning fra PDL.

--- src/main/kotlin/no/nav/samordning/interceptor/AzureAdTokenRequestInterceptor.kt
+++ src/main/kotlin/no/nav/samordning/interceptor/AzureAdTokenRequestInterceptor.kt
@@ -3,11 +3,12 @@ package no.nav.samordning.interceptor
 import no.nav.common.token_client.client.AzureAdMachineToMachineTokenClient
 import no.nav.samordning.config.RestTemplateConfig.PdlInterceptor
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpRequest
+import org.springframework.http.HttpStatus
 import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
+import org.springframework.web.server.ResponseStatusException
 
 class AzureAdTokenRequestInterceptor(private val client : AzureAdMachineToMachineTokenClient, private val scope: String): ClientHttpRequestInterceptor {
 
@@ -15,25 +16,17 @@ class AzureAdTokenRequestInterceptor(private val client : AzureAdMachineToMachin
 
     override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
 
-        logger.debug("Fetching MachineToMachine token using scope: $scope")
-        val token = client.createMachineToMachineToken(scope)
+        val token = try {
+            logger.debug("Fetching MachineToMachine token using scope: $scope")
+            client.createMachineToMachineToken(scope) ?: throw Exception("failed to fetch token")
+        } catch (ex: Exception) {
+            logger.error("Failed to fetch token from scope", ex)
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to fetch token from scope")
+        }
         logger.debug("Token is set ${token != null}")
 
-        if (request.headers[HttpHeaders.AUTHORIZATION] == null) {
-            logger.debug("Authorization header is null")
-            request.headers.setBearerAuth(token)
-
-            if (request.headers[HttpHeaders.AUTHORIZATION] != null) {
-                logger.debug("Authorization header is set using token")
-            } else {
-                logger.warn("Authorization header is still missing")
-                request.headers.setBearerAuth(client.createMachineToMachineToken(scope))
-                logger.debug("Authorization header is set again using token")
-            }
-
-        } else {
-            logger.debug("Authorization header exists")
-        }
+        request.headers.setBearerAuth(token)
+        logger.debug("Authorization Token is set on headers")
 
         return execution.execute(request, body)
     }
