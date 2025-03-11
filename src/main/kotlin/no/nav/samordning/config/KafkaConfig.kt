554409bb762b36d0dee233e34063d78a6d7f6790
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2025-03-11 08:18:13 +0100

    PES-604: Få på plass dependencies for kafka-avro, KafkaConfig.

---
+++ src/main/kotlin/no/nav/samordning/config/KafkaConfig.kt
@@ -0,0 +1,75 @@
+package no.nav.samordning.config
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import no.nav.person.pdl.leesah.Personhendelse
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.config.SslConfigs
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.ContainerProperties
+import java.time.Duration
+
+@EnableKafka
+@Profile("prod", "dev")
+@Configuration
+class KafkaConfig(
+    @param:Value("\${kafka.keystore.path}") private val keystorePath: String,
+    @param:Value("\${kafka.credstore.password}") private val credstorePassword: String,
+    @param:Value("\${kafka.truststore.path}") private val truststorePath: String,
+    @param:Value("\${kafka.brokers}") private val aivenBootstrapServers: String,
+    @param:Value("\${kafka.security.protocol}") private val securityProtocol: String,
+    @param:Value("\${kafka.sporingslogg.aivengroupid}") private val aivenGroupid: String,
+) {
+
+    fun aivenKafkaConsumerFactory(): ConsumerFactory<Any, Any?> {
+        val configMap: MutableMap<String, Any> = HashMap()
+        aivenCommonConfig(configMap)
+        commonConfig(aivenBootstrapServers, aivenGroupid, configMap)
+
+        val avroValueDes = KafkaAvroDeserializer()
+        val avroKeyDes = KafkaAvroDeserializer().apply {
+            configure(configMap, false)
+        }
+
+        return DefaultKafkaConsumerFactory(configMap, avroKeyDes, avroValueDes)
+    }
+
+
+    @Bean
+    fun aivenKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Personhendelse> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Personhendelse>()
+        factory.consumerFactory = aivenKafkaConsumerFactory()
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
+        factory.containerProperties.setAuthExceptionRetryInterval(Duration.ofSeconds(4L))
+        return factory
+    }
+
+    private fun commonConfig(bootstrapServer: String, groupid: String, configMap: MutableMap<String, Any>) {
+        configMap[ConsumerConfig.CLIENT_ID_CONFIG] = groupid
+        configMap[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServer
+        configMap[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
+        configMap[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        configMap[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = 1
+    }
+
+
+    private fun aivenCommonConfig(configMap: MutableMap<String, Any>) {
+        configMap[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = keystorePath
+        configMap[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEY_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG] = "JKS"
+        configMap[SslConfigs.SSL_KEYSTORE_TYPE_CONFIG] = "PKCS12"
+        configMap[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststorePath
+        configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = securityProtocol
+    }
+
+
+}
