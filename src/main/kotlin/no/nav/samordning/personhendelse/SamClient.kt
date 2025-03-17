c3bf35d2f091ddd3b6b1eb5127aaa55b1739c229
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2025-03-17 10:58:23 +0100

    PES-605: logging i SamClient

--- src/main/kotlin/no/nav/samordning/personhendelse/SamClient.kt
+++ src/main/kotlin/no/nav/samordning/personhendelse/SamClient.kt
@@ -1,5 +1,7 @@
 package no.nav.samordning.personhendelse
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
@@ -7,15 +9,20 @@ import org.springframework.web.client.RestTemplate
 @Service
 class SamClient(private val samRestTemplate: RestTemplate) {
 
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
     fun oppdaterSamPersonalia(path: String, request: OppdaterPersonaliaRequest) {
 
         try {
+            val urlpath = "/api/oppdaterpersonalia/$path"
+            logger.debug("kaller opp : $urlpath")
             samRestTemplate.postForEntity(
-                "/api/oppdaterpersonalia/$path",
+                urlpath,
                 request,
                 Unit::class.java
             )
         } catch (re: RestClientException) {
+            logger.error("Kall mot SAM oppdaterpersonalia $path feilet", re)
             throw Exception("Kall mot SAM oppdaterpersonalia $path feilet", re)
         }
 
