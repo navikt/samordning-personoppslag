074c5873485969fe612f1fd35a944b8b5b31b635
Morten Normann <morten.normann@nav.no>
2025-03-26 14:37:23 +0100

    Begynt på hentAdresse

--- src/main/kotlin/no/nav/samordning/personhendelse/OppdaterPersonaliaRequest.kt
+++ src/main/kotlin/no/nav/samordning/personhendelse/OppdaterPersonaliaRequest.kt
@@ -22,7 +22,8 @@ data class PersonData(
     val sivilstand: String? = null,
     val sivilstandDato: LocalDate? = null,
     val dodsdato: LocalDate? = null,
-    val adressebeskyttelse: List<AdressebeskyttelseGradering> = emptyList()
+    val adressebeskyttelse: List<AdressebeskyttelseGradering> = emptyList(),
+    val bostedsAdresse: Bostedsadresse? = null,
 ) {
     override fun toString(): String {
         return """
@@ -34,3 +35,21 @@ data class PersonData(
         """.trimIndent()
     }
 }
+
+data class Bostedsadresse(
+    val boadresse1: String? = null,
+    val boadresse2: String? = null,
+    val bolignr: String? = null,
+    val kommunenr: String? = null,
+    val navenhet: String? = null,
+    val postAdresse: Tilleggsadresse? = null,
+    val tilleggsAdresse: Tilleggsadresse? = null,
+    val utenlandsAdresse: Tilleggsadresse? = null,
+)
+
+data class Tilleggsadresse(
+    val adresselinje1: String? = null,
+    val adresselinje2: String? = null,
+    val adresselinje3: String? = null,
+    val landkode: String? = null,
+)
