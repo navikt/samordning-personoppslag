6991ed97879c40211252a38ff303e9c7feba9e4e
Morten Normann <morten.normann@nav.no>
2026-02-09 12:59:48 +0100

    Legg på ProtectedWithClaims på example sivilstand-endepunktet
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>

--- src/main/kotlin/no/nav/samordning/personhendelse/examples/HendelseExampleController.kt
+++ src/main/kotlin/no/nav/samordning/personhendelse/examples/HendelseExampleController.kt
@@ -1,13 +1,21 @@
 package no.nav.samordning.personhendelse.examples
 
+import no.nav.samordning.person.PersonRequest
+import no.nav.samordning.person.pdl.PersonoppslagException
+import no.nav.samordning.person.sam.PersonSamordningService
+import no.nav.samordning.person.sam.model.Person
 import no.nav.samordning.personhendelse.PersonEndringHendelseProducer
 import no.nav.samordning.personhendelse.Meldingskode
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDate
 import java.util.UUID
 
@@ -17,13 +25,17 @@ import java.util.UUID
  */
 @RestController
 @RequestMapping("/api/hendelse/example")
-class HendelseExampleController(private val personEndringHendelseProducer: PersonEndringHendelseProducer) {
+class HendelseExampleController(
+    private val personEndringHendelseProducer: PersonEndringHendelseProducer,
+    private val personSamordningService: PersonSamordningService,
+) {
 
     /**
      * Publiserer en sivilstandsendring hendelse
      * Eksempel: POST /api/hendelse/example/sivilstand?fnr=12345678901&tpNr=1000&tpNr=2000&sivilstand=GIFT&sivilstandDato=2025-01-15
      */
     @PostMapping("/sivilstand")
+    @ProtectedWithClaims("entraid")
     fun publiserSivilstandsendring(
         @RequestParam fnr: String,
         @RequestParam tpNr: List<String>,
@@ -39,6 +51,18 @@ class HendelseExampleController(private val personEndringHendelseProducer: Perso
             hendelseId = UUID.randomUUID().toString()
         )
         return ResponseEntity.ok().build()
+
+    }
+
+
+    @PostMapping("/person")
+    @ProtectedWithClaims("entraid")
+    fun hentPerson(@RequestBody request: PersonRequest) : ResponseEntity<Person> {
+        try {
+            return ResponseEntity.ok().body(personSamordningService.hentPerson(request.fnr))
+        } catch (pe: PersonoppslagException) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, pe.message)
+        }
 
     }
 
