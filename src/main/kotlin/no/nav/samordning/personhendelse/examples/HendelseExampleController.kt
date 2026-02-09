831395fbc9f46c50de7ae76619f768f251c13e7e
Morten Normann <morten.normann@nav.no>
2026-02-09 13:33:35 +0100

    Kall PersonEndringHendelseService i HendelseExampleController
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>

--- src/main/kotlin/no/nav/samordning/personhendelse/examples/HendelseExampleController.kt
+++ src/main/kotlin/no/nav/samordning/personhendelse/examples/HendelseExampleController.kt
@@ -4,11 +4,10 @@ import no.nav.samordning.person.PersonRequest
 import no.nav.samordning.person.pdl.PersonoppslagException
 import no.nav.samordning.person.sam.PersonSamordningService
 import no.nav.samordning.person.sam.model.Person
-import no.nav.samordning.personhendelse.PersonEndringHendelseProducer
 import no.nav.samordning.personhendelse.Meldingskode
+import no.nav.samordning.personhendelse.PersonEndringHendelseService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -26,8 +25,7 @@ import java.util.UUID
 @RestController
 @RequestMapping("/api/hendelse/example")
 class HendelseExampleController(
-    private val personEndringHendelseProducer: PersonEndringHendelseProducer,
-    private val personSamordningService: PersonSamordningService,
+    private val personEndringHendelseService: PersonEndringHendelseService,
 ) {
 
     /**
@@ -42,8 +40,7 @@ class HendelseExampleController(
         @RequestParam sivilstand: String,
         @RequestParam sivilstandDato: LocalDate
     ): ResponseEntity<Any>? {
-        personEndringHendelseProducer.publiserPersonEndringHendelse(
-            tpNr = tpNr,
+        personEndringHendelseService.opprettPersonEndringHendelse(
             fnr = fnr,
             meldingsKode = Meldingskode.SIVILSTAND,
             sivilstand = sivilstand,
@@ -51,57 +48,6 @@ class HendelseExampleController(
             hendelseId = UUID.randomUUID().toString()
         )
         return ResponseEntity.ok().build()
-
-    }
-
-
-    @PostMapping("/person")
-    @ProtectedWithClaims("entraid")
-    fun hentPerson(@RequestBody request: PersonRequest) : ResponseEntity<Person> {
-        try {
-            return ResponseEntity.ok().body(personSamordningService.hentPerson(request.fnr))
-        } catch (pe: PersonoppslagException) {
-            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, pe.message)
-        }
-
-    }
-
-    /**
-     * Publiserer en fødselsnummer endring hendelse
-     * Eksempel: POST /api/hendelse/example/fodselsnummer?fnr=12345678901&oldFnr=98765432109&tpNr=1000
-     */
-    @PostMapping("/fodselsnummer")
-    fun publiserFodselsnummerEndring(
-        @RequestParam fnr: String,
-        @RequestParam oldFnr: String,
-        @RequestParam tpNr: List<String>
-    ) {
-        personEndringHendelseProducer.publiserPersonEndringHendelse(
-            tpNr = tpNr,
-            fnr = fnr,
-            oldFnr = oldFnr,
-            meldingsKode = Meldingskode.FODSELSNUMMER,
-            hendelseId = UUID.randomUUID().toString()
-        )
-    }
-
-    /**
-     * Publiserer en dødsfall hendelse
-     * Eksempel: POST /api/hendelse/example/doedsfall?fnr=12345678901&tpNr=1000&dodsdato=2025-01-10
-     */
-    @PostMapping("/doedsfall")
-    fun publiserDoedsfallHendelse(
-        @RequestParam fnr: String,
-        @RequestParam tpNr: List<String>,
-        @RequestParam dodsdato: LocalDate
-    ) {
-        personEndringHendelseProducer.publiserPersonEndringHendelse(
-            tpNr = tpNr,
-            fnr = fnr,
-            meldingsKode = Meldingskode.DOEDSFALL,
-            dodsdato = dodsdato,
-            hendelseId = UUID.randomUUID().toString()
-        )
 
     }
 }
