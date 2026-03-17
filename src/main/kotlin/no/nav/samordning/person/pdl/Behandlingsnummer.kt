1d8bd5f6f864eaf5d7afd4f554afafb6a86bad47
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2026-03-17 12:18:05 +0100

    Fjerne bruk av Behandlingsnummer 255 Uføre for kall til PDL.

--- src/main/kotlin/no/nav/samordning/person/pdl/Behandlingsnummer.kt
+++ src/main/kotlin/no/nav/samordning/person/pdl/Behandlingsnummer.kt
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.annotation.JsonValue
 /**
  * https://behandlingskatalog.intern.nav.no/process/team/5967460b-e9bd-4e63-9544-db209136c867/d0cf6f79-3c8b-4b35-91bf-5b1e74516fb0
  * B920, Samordningsloven samordning med offentlige tjenestepensjon
- * B255, Uføretrygd: Saksbehandling og forvaltning av ytelsen
  * B137, Forvaltning av registere: Innsamling, registrering og lagring i Tjenestepensjonsregisteret
  */
 enum class Behandlingsnummer(@JsonValue val nummer: String) {
       TP("B137"),
-      UFOREP("B255"),
       SAMORDNING("B920");
 
     companion object {
